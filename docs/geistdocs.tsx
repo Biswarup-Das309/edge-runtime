836144ca2e05c6377c0618706a499e3766334a8b
Hayden Bleasel <hello@haydenbleasel.com>
2025-12-09 18:27:44 -0800

    Update geistdocs.tsx

--- docs/geistdocs.tsx
+++ docs/geistdocs.tsx
@@ -1,48 +1,51 @@
-import { BookHeartIcon } from "lucide-react";
+import { BookHeartIcon } from 'lucide-react'
 
 export const Logo = () => (
-  <div className="flex items-center gap-2">
-    <BookHeartIcon className="size-5" />
-    <p className="font-semibold text-xl tracking-tight">Geistdocs</p>
+  <div className='flex items-center gap-2'>
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 172 158'
+      width={20}
+      height={20}
+      fill='none'
+    >
+      <path d='m96.71 0 43.999 120-50-80-50 10 56-50Z' fill='currentColor' />
+      <path
+        d='M171.083 148.881 40.209 120h100.5l14-40 16.374 68.881Z'
+        fill='currentColor'
+      />
+      <path
+        d='M0 129.54 90.71 40l-50 80 26.868 37.112L0 129.54Z'
+        fill='currentColor'
+      />
+    </svg>
+    <p className='font-semibold text-xl tracking-tight'>Edge Runtime</p>
   </div>
-);
+)
 
 export const github = {
-  owner: undefined as string | undefined,
-  repo: undefined as string | undefined,
-};
+  owner: 'vercel',
+  repo: 'edge-runtime',
+}
 
-export const nav = [
-  {
-    label: "Docs",
-    href: "/docs",
-  },
-  {
-    label: "Source",
-    href: `https://github.com/${github.owner}/${github.repo}/`,
-  },
-];
+export const nav = []
 
 export const suggestions = [
-  "What is Vercel?",
-  "What can I deploy with Vercel?",
-  "What is Fluid Compute?",
-  "How much does Vercel cost?",
-];
+  'What is Edge Runtime?',
+  'What are the available APIs?',
+  'What packages are available?',
+  'How do I use the Edge Runtime?',
+]
 
-export const title = "Geistdocs Documentation";
+export const title = 'Edge Runtime Documentation'
 
 export const prompt =
-  "You are a helpful assistant specializing in answering questions about Geistdocs, a modern documentation template built with Next.js and Fumadocs.";
+  'You are a helpful assistant specializing in answering questions about Edge Runtime, which is designed to help framework authors adopt edge computing and provide open-source tooling built on Web standards. It’s designed to be integrated into frameworks (like Next.js) and not for usage in application code.'
 
 export const translations = {
   en: {
-    displayName: "English",
+    displayName: 'English',
   },
-  cn: {
-    displayName: "Chinese",
-    search: "搜尋文檔",
-  },
-};
+}
 
-export const basePath: string | undefined = undefined;
+export const basePath: string | undefined = undefined
